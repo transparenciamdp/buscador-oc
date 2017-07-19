1069591d22c9b05c5bc5720a72cbef66784bc92e
tincho <martin.salinas@gmail.com>
2017-07-19 08:39:45 -0300

    latest react-cs changes include fieldFormatters

--- app.jsx
+++ app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchApp from './react-customsearch/app.jsx';
 
+const currency = v => "$ " + parseFloat(v);
+
 let rootElement = document.querySelector("#search-form");
 let
     src = rootElement.attributes["api-src"].value || "/",
@@ -17,9 +19,13 @@ let
         unidadmedida: "U.M",
         preciototal: "Precio total"
     },
+    fieldFormatters = {
+        preciounitario: currency,
+        preciototal: currency
+    },
     limit = 20;
 
 // @TODO it might be good a "resilient" behavior where first raw columnNames are shown
 // then "translations" / displayable text may come from an XHRequest or other source async'ly, and update
 
-ReactDOM.render(<SearchApp options={{src, columnNames, limit}} />, rootElement);
+ReactDOM.render(<SearchApp options={{src, columnNames, limit, fieldFormatters}} />, rootElement);
