4dce33aac1b786c932235dcddfa0459add4142ed
Martin Salinas <msalinas@quadiontech.com>
2017-07-19 13:31:45 -0300

    formatter for fechaemision

--- app.jsx
+++ app.jsx
@@ -21,11 +21,17 @@ let
     },
     fieldFormatters = {
         preciounitario: currency,
-        preciototal: currency
+        preciototal: currency,
+        fechaemision: d => {
+          if (d === "1900-01-01T00:00:00.000Z") {
+            return "Sin especificar";
+          }
+          return new Date(d).toISOString().slice(0,10);
+        }
     },
     limit = 20;
 
-// @TODO it might be good a "resilient" behavior where first raw columnNames are shown
+// @TODO about column headers, it might be good a "resilient" behavior where first raw columnNames are shown
 // then "translations" / displayable text may come from an XHRequest or other source async'ly, and update
 
 ReactDOM.render(<SearchApp options={{src, columnNames, limit, fieldFormatters}} />, rootElement);
